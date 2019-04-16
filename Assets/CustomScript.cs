4d6ba9052b1558d71203b602a9d6dde4955f0b7f
William Botura <williamdigitalwork@gmail.com>
2019-04-16 11:48:26 -0300

    Alterações
    Alterações no Script base do Vuforia, configurando Scripts para o uso de UnityEvents, e configuração de custom Script com Unity Evets

---
+++ Assets/CustomScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomScript : MonoBehaviour
+{
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    public void ExibirInfoConsole(string info)
+    {
+        Debug.Log(info);
+    }
+}
